b5497d4eaf99a7c29bc7013e99781ee4588750eb
noopuram <noopuram_in@yahoo.com>
2018-08-08 16:21:22 -0700

    Assesment

--- Week-1.js
+++ Week-1.js
@@ -1,8 +1,26 @@
-// 1. Write an anonymous function that takes one argument of type number and decides if that number is evenly divisble by three or not. If it is, print the number and "is divisible by three". If it is not, print that the number "is not divisble by three".
+function result(a) {
+  if (a / 3 === 0) {
+    console.log("Divisable by three");
+  } else {
+    console.log("Not divisible by three");
+  }
+}
+// return result
+//function call
 
-// 2. Write about yourself using an object. Include at least three properties of you and store your object in a variable with your name.
+result(8)
 
-// 3. Create an array with at least 4 items inside it. Show how to access two values from the array in two different ways.
+var person = {name:"Sunil" , age:41 , nationality:"Indian",
+getdata: function(){return this.name+ "-" + this.age+ "-" +this.nationality}};
+person.name
+person.age
+person.nationality
+
+console.log(person.getdata());. Write about yourself using an object. Include at least three properties of you and store your object in a variable with your name.
+
+var names = ["mat" , "jack" , "pat" , "lee"];
+console.log(names[0]);
+console.log(names);
 
 // 4. Have the function AlphabetSoup(str) take the "str" parameter being passed and return a string with the letters in alphabetical order (ie. hello becomes ehllo). Assume no numbers or punctuation symbols will not be included in the parameter. 
 
@@ -20,10 +38,10 @@ function AlphabetSoup(str) {
 
 AlphabetSoup(readline());   
 
-// 5. Given the arrays below, use a for loop to print one value from each array concatenated together. How would your code need to change to accomodate arrays of different lengths?
-
-var nums = [1, 5, 88, 2, 5, 42, 57, 101]
-
+var nums = [1,5,88,2,5,42,57,101]
 var nouns = ["ducks", "telephone booth", "the enterprise", "robots", "amazon", "eraser", "zafod", "a"]
+var out = ""
+for (var i =0; i<nums.length; i++) {out+= nums[i] + ":" + nouns [i] + " , ";
 
-// output of the first function should be: "1 ducks"
+}
+console.log(out);
